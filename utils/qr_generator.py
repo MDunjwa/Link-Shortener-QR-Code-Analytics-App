f10bd50439abac58391c00457b659f3b801db81c
Mbasa Dunjwa <mbdunjwcpt024@student.wethinkcode.co.za>
2025-11-03 22:02:52 +0200

    Docstrings and name changes

--- utils/qr_generator.py
+++ utils/qr_generator.py
@@ -2,7 +2,15 @@ import qrcode
 import os
 from datetime import datetime
 
-def generate_qr(short_url: str, filename: str = None):
+"""
+Takes in a URL and generates a QR code that leads to it
+
+Arguments: url (string) the URL being shared
+           filename (str) the optional filename we want for the qr code image
+
+Returns: path (str) the location where the QR code image was saved
+"""
+def generate_qr_code(url: str, filename: str = None):
     if filename is None:
         filename = f"qr_{int(datetime.now().timestamp())}.png"
 
@@ -11,7 +19,7 @@ def generate_qr(short_url: str, filename: str = None):
 
     path = os.path.join(folder, filename)
 
-    img = qrcode.make(short_url)
+    img = qrcode.make(url)
     img.save(path)
 
     return path
