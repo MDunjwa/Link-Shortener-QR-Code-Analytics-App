d2e4f3a2d000f270b1fa7daec7703c0c5c5a2381
Mbasa Dunjwa <mbasaemail@gmail.com>
2025-11-03 23:32:45 +0200

    Refactored qrcode function to not output image files

--- utils/qr_generator.py
+++ utils/qr_generator.py
@@ -1,26 +1,22 @@
 import qrcode
-import os
-from datetime import datetime
+
+# For saving qr codes as base64 strings instead of images
+import io
+import base64
 
 """
 Takes in a URL and generates a QR code that leads to it
 
 Arguments: url (string) the URL being shared
-           filename (str) the optional filename we want for the qr code image
 
-Returns: path (str) the location where the QR code image was saved
+Returns: base64_string (str) the base64_string version of the qr code image created, which our HTML will display
 """
-def generate_qr_code(url: str, filename: str = None):
-    if filename is None:
-        filename = f"qr_{int(datetime.now().timestamp())}.png"
-
-    folder = os.path.join("static", "qrcodes")
-    os.makedirs(folder, exist_ok=True)
-
-    path = os.path.join(folder, filename)
-
+def generate_qr_code(url: str, filename: str = None):   
     img = qrcode.make(url)
-    img.save(path)
+    buffer_file = io.BytesIO() # I'm creating file in memory instead now
+    img.save(buffer_file, format='PNG')
+    
+    image_bytes = buffer_file.getvalue() # I'm getting the raw image data from the buffer file in memory
+    base64_string = base64.b64encode(image_bytes).decode() # I'm putting the bytes in base64 format then converting them to a regular string for json
 
-    return path
-#testing github contributions
+    return base64_string
